2d4672723e4f87cf558384f1dce403ba85e4e900
Sean <gilmore.seven@gmail.com>
2016-11-09 14:54:32 +1100

    deployment to modulus complete

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ app.get('/',function(req,res){
   //__dirname : It will resolve to your project folder.
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Example app listening on port' + port+ '!');
 });
-
-
